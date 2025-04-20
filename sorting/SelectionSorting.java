cfcdc1950a8154f27e4edb5e0ab9cd1dd4fd3191
Dev Kumar <devkmandal0@gmail.com>
2025-04-20 12:07:53 +0530

    Learnt Selection Sorting

---
+++ sorting/SelectionSorting.java
@@ -0,0 +1,30 @@
+package sorting;
+
+public class SelectionSorting {
+    public static void main(String[] args) {
+
+        int[] arr = {1,2,6,8,0,5,9,3};
+        int n=arr.length;
+
+        for (int i = 0; i <=n-2 ; i++) {
+            int min=i;
+            for (int j = i; j <=n-1 ; j++) {
+                if(arr[j]<arr[min])min=j;
+            }
+
+            int temp =arr[i];
+            arr[i]=arr[min];
+            arr[min]=temp;
+
+
+          
+
+        }
+
+        for(int num:arr){
+            System.out.print(num+ " ");
+        }
+
+
+    }
+}
