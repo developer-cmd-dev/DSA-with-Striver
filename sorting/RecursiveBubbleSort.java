6f774b68ff6772566cf3aed30199796191e33a5d
Dev Kumar <devkmandal0@gmail.com>
2025-04-21 11:52:17 +0530

    Solved BubbleSort using recursion

---
+++ sorting/RecursiveBubbleSort.java
@@ -0,0 +1,30 @@
+package sorting;
+
+public class RecursiveBubbleSort {
+    public static void main(String[] args) {
+
+        int[] arr={3,4,2,6,8,9};
+        int[] result = recurBS(arr,0,arr.length);
+        for(int num:result){
+            System.out.print(num+" ");
+        }
+
+
+    }
+
+
+    public static int[] recurBS(int[] arr,int i,int n){
+        if(i==n-1) return  arr;
+
+        recurBS(arr,i+1,n);
+        for (int j = 0; j <=i ; j++) {
+            if(arr[j]>arr[j+1]){
+                int temp = arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+
+        return arr;
+    }
+}
