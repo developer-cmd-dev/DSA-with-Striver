8da4658f2db00a69bb57418a7390f6c9e2d0caa1
Dev Kumar <devkmandal0@gmail.com>
2025-04-21 13:22:22 +0530

    solving Insertion sorting problem using Recurison

---
+++ sorting/RecursiveInsertion.java
@@ -0,0 +1,25 @@
+package sorting;
+
+public class RecursiveInsertion {
+    public static void main(String[] args) {
+        int[] arr={3,4,9,8,2,0};
+        int[] result = recursiveIns(arr,0,arr.length);
+        for(int num:result){
+            System.out.print(num+" ");
+        }
+
+    }
+
+    public static int[] recursiveIns(int arr[],int i,int n){
+        if(i==n) return arr;
+        recursiveIns(arr,i+1,n);
+        int j=i;
+        while(j>0&&arr[j-1]>arr[j]){
+            int temp = arr[j];
+            arr[j]=arr[j-1];
+            arr[j-1]=temp;
+            j--;
+        }
+        return arr;
+    }
+}
