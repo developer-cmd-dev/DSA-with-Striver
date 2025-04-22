076a50ddbc1e36483c0332d31bc9c2ed65bdc88a
Dev Kumar <devkmandal0@gmail.com>
2025-04-22 12:20:44 +0530

    Learnt Cyclic Sort

---
+++ sorting/CycleSort.java
@@ -0,0 +1,29 @@
+package sorting;
+
+public class CycleSort {
+
+    public static void main(String[] args) {
+
+        int[] arr = {2,3,5,4,1,6};
+        for (int num:cyclicSort(arr,0,arr.length)){
+            System.out.print(num+" ");
+        }
+
+
+    }
+
+    public static int[] cyclicSort(int[] arr,int i,int n){
+        while(i<n){
+            int correctIndex = arr[i]-1;
+           if(arr[i]!=arr[correctIndex]){
+               int temp = arr[i];
+               arr[i]=arr[correctIndex];
+               arr[correctIndex]=temp;
+           }else {
+               i++;
+           }
+
+        }
+        return arr;
+    }
+}
