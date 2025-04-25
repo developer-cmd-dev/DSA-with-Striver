68c47abd650cb76fd81973c970f2a339ce2d3f95
Dev Kumar <devkmandal0@gmail.com>
2025-04-25 09:07:35 +0530

    Solved Largest Number in array

--- Arrays/LargestElementINArray.java
+++ Arrays/LargestElementINArray.java
@@ -1,4 +1,17 @@
 package Arrays;
 
 public class LargestElementINArray {
+    public static void main(String[] args) {
+    int[] arr = {1,4,5,8,7,9};
+        System.out.println(findLargestNum(arr));
+    }
+
+    public static int findLargestNum(int[] arr){
+        int largest =arr[0];
+        for (int i = 0; i <arr.length ; i++) {
+            if(arr[i]>largest) largest=arr[i];
+
+        }
+        return largest;
+    }
 }
