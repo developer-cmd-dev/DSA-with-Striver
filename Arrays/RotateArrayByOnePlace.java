75fe701cc32c1dd4e3eabdb213469e2f463557c2
Dev Kumar <devkmandal0@gmail.com>
2025-04-25 10:46:23 +0530

    Solve array rotation by one place

--- Arrays/RotateArrayByOnePlace.java
+++ Arrays/RotateArrayByOnePlace.java
@@ -6,13 +6,27 @@ public class RotateArrayByOnePlace {
     public static void main(String[] args) {
 
         int[] arr={1,2,3,4,5,6};
-        int temp = arr[0];
-         arr[0]=arr[arr.length-1];
-        arr[arr.length-1]=temp;
-        System.out.println(Arrays.toString(arr));
+
+        System.out.println(Arrays.toString(arrayRotation(arr,arr.length,3)));
 
 
 
+    }
+
+    public static int[] arrayRotation(int[] arr,int n,int rotation){
+
+        int j=0;
+       while(j<rotation){
+                int firstElem = arr[0];
+                for (int i = 1; i <n ; i++) {
+                    arr[i-1]=arr[i];
+                    arr[i]=firstElem;
+
+                }
+            j++;
+        }
+
+        return  arr;
     }
 
 
