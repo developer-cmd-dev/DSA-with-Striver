ec752acb7b677428f4c835fcca752ee994ba0929
Dev Kumar <devkmandal0@gmail.com>
2025-05-02 13:34:49 +0530

    Solved using revers method of optimal solution

--- PracticeQuestions/RotateArrayByDPlace.java
+++ PracticeQuestions/RotateArrayByDPlace.java
@@ -9,7 +9,8 @@ public class RotateArrayByDPlace {
 
         int[] arr={1,2,3,4,5,6,7};
         int d=3;
-        rotateArr(arr,arr.length,d);
+//        rotateArr(arr,arr.length,d);
+        rotateArrayOptimal(arr,arr.length,d);
 
 
     }
@@ -34,6 +35,28 @@ public class RotateArrayByDPlace {
 
 
         System.out.println("Brute Force: "+ Arrays.toString(arr));
+
+    }
+
+
+
+    public static void rotateArrayOptimal(int[] arr,int n,int d){
+            reverseArr(arr,0,d-1);
+            reverseArr(arr,d,n-1);
+            reverseArr(arr,0,n-1);
+        System.out.println("Optimal Approach: "+ Arrays.toString(arr));
+
+    }
+
+
+    public static void reverseArr(int[] arr,int left,int right){
+      while(left<right){
+          int temp=arr[left];
+          arr[left]=arr[right];
+          arr[right]=temp;
+          left++;
+          right--;
+      }
     }
 
 
