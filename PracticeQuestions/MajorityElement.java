07c2b325720b17fad9acdf38cae5a908a4ef34ad
Dev Kumar <devkmandal0@gmail.com>
2025-05-08 11:19:01 +0530

    Solved using better approach

---
+++ PracticeQuestions/MajorityElement.java
@@ -0,0 +1,33 @@
+package PracticeQuestions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MajorityElement {
+    public static void main(String[] args) {
+        int[] arr={2,2,1,3,1,1,3,1,1};
+        System.out.println(betterApproach(arr,arr.length));
+
+    }
+
+    public static int betterApproach(int[] arr,int n){
+        Map<Integer,Integer>map=new HashMap<>();
+        int target = n/2;
+
+        for (int i = 0; i <n ; i++) {
+            if(!map.containsKey(arr[i])){
+                map.put(arr[i],1);
+            }else{
+                map.put(arr[i],map.get(arr[i])+1);
+            }
+            
+        }
+
+        for (Map.Entry<Integer,Integer>value:map.entrySet()){
+            if(value.getValue()>target){
+                return value.getKey();
+            }
+        }
+        return -1;
+    }
+}
