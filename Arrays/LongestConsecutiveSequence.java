87313f21a69d4a61e42907c8287e68e5248edfb6
Dev Kumar <devkmandal0@gmail.com>
2025-05-14 12:27:28 +0530

    Solved using better approach

--- Arrays/LongestConsecutiveSequence.java
+++ Arrays/LongestConsecutiveSequence.java
@@ -1,10 +1,14 @@
 package Arrays;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class LongestConsecutiveSequence {
     public static void main(String[] args) {
 
         int[] arr={102,4,100,1,101,3,2,1,1};
         bruteForce(arr,arr.length);
+        betterApproach(arr,arr.length);
     }
 
 
@@ -33,5 +37,23 @@ public class LongestConsecutiveSequence {
             
         }
         return false;
+    }
+
+
+    public static void betterApproach(int[] arr,int n){
+        Arrays.sort(arr); // Sorting is important;
+        int longest=1,count=0,lastSmaller=Integer.MIN_VALUE;
+        for (int i = 0; i <n ; i++) {
+            if(arr[i]-1==lastSmaller){
+                count++;
+                lastSmaller=arr[i];
+            }else if (arr[i]!=lastSmaller){
+                count=1;
+                lastSmaller=arr[i];
+            }
+            if(count>longest)longest=count;
+        }
+
+        System.out.println(longest);
     }
 }
