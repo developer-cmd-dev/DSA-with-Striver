9ad4539e64bc811de6a080d58ba04620e7b18f45
Dev Kumar <devkmandal0@gmail.com>
2025-05-14 14:27:06 +0530

    Solved using brute force

---
+++ Arrays/SetMatrixZeros.java
@@ -0,0 +1,65 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class SetMatrixZeros
+{
+    public static void main(String[] args) {
+
+        int[][] arr={
+                {1,1,1,1},
+                {1,0,0,1},
+                {1,1,0,1},
+                {1,1,1,1}
+        };
+
+        bruteForce(arr,arr.length,arr[0].length);
+
+
+
+    }
+
+    public static void bruteForce(int[][] arr,int n,int m){
+
+        for (int i = 0; i <n ; i++) {
+            for (int j = 0; j <m ; j++) {
+                if(arr[i][j]==0){
+                    markRow(arr,i,m);
+                    markCol(arr,j,n);
+                }
+
+            }
+            
+        }
+
+        for (int i = 0; i <n ; i++) {
+            for (int j = 0; j <m ; j++) {
+                if(arr[i][j]==-1)arr[i][j]=0;
+
+            }
+
+        }
+
+        System.out.println(Arrays.deepToString(arr));
+    }
+
+
+    public static void markRow(int[][] arr,int i,int m){
+
+        for (int j = 0; j <m ; j++) {
+            if(arr[i][j]!=0)arr[i][j]=-1;
+        }
+
+    }
+
+    public static void markCol(int[][] arr,int j,int n){
+
+
+        for (int i = 0; i < n; i++) {
+            if(arr[i][j]!=0)arr[i][j]=-1;
+        }
+
+        
+
+    }
+}
