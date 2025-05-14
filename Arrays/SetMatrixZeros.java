ed24f766dea7dc9786f2ca88745659e2ae6b2c5e
Dev Kumar <devkmandal0@gmail.com>
2025-05-14 15:33:49 +0530

    Solved using optimal approach

--- Arrays/SetMatrixZeros.java
+++ Arrays/SetMatrixZeros.java
@@ -13,8 +13,13 @@ public class SetMatrixZeros
                 {1,1,1,1}
         };
 
+
+        int[][] matrix={{1,1,1,1},{1,0,1,1},{1,1,0,1},{0,1,1,1}};
+
 //        bruteForce(arr,arr.length,arr[0].length);
-        bruteForce(arr,arr.length,arr[0].length);
+//        betterApro(arr,arr.length,arr[0].length);
+//        optimalApproach(matrix,matrix.length,matrix[0].length);
+        optimalApproach(arr,arr.length,arr[0].length);
 
 
 
@@ -93,5 +98,49 @@ public class SetMatrixZeros
         System.out.println(Arrays.deepToString(arr));
     }
 
+
+    public static void optimalApproach(int[][] arr,int n,int m){
+
+        int col0=1;
+
+        for (int i = 0; i <n ; i++) {
+            for (int j = 0; j <m ; j++) {
+                if(arr[i][j]==0){
+                    arr[i][0]=0;
+                    if(j!=0){
+                        arr[0][j]=0;
+                    }else{
+                        col0=0;
+                    }
+                }
+
+
+            }
+
+        }
+
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                if(arr[i][0]==0||arr[0][j]==0){
+                    arr[i][j]=0;
+                }
+            }
+        }
+
+        if(arr[0][0]==0){
+            for(int j=0;j<m;j++){
+                arr[0][j]=0;
+            }
+        }
+
+        if(col0==0){
+            for(int i=0;i<n;i++){
+                arr[i][0]=0;
+            }
+        }
+
+        System.out.println(Arrays.deepToString(arr));
+
+    }
 
 }
