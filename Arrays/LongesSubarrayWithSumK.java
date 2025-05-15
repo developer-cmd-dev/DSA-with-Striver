f5c50841018d971ab71aa23ad7abc4377ff57efd
Dev Kumar <devkmandal0@gmail.com>
2025-05-15 10:31:24 +0530

    Solved using optimal approach

--- Arrays/LongesSubarrayWithSumK.java
+++ Arrays/LongesSubarrayWithSumK.java
@@ -4,7 +4,8 @@ public class LongesSubarrayWithSumK {
     public static void main(String[] args) {
 
         int[] arr={1,2,3,1,1,1,1,4,2,3};
-        bruteForce(arr,arr.length,3);
+        bruteForce(arr,arr.length,8);
+        optimalAppr(arr,arr.length,3);
 
     }
 
@@ -22,5 +23,28 @@ public class LongesSubarrayWithSumK {
         }
 
         System.out.println(longest);
+    }
+
+    public static void optimalAppr(int[] arr,int n,int target){
+
+    int maxLen=0,i=0,j=0,sum=0;
+    while (j<n){
+
+        while(sum>target){
+            sum-=arr[i];
+            i++;
+        }
+
+        if(sum==target){
+            maxLen = Math.max(maxLen,j-i+1);
+        }
+        j++;
+        if(j<n)sum+=arr[j];
+
+    }
+
+        System.out.println(maxLen);
+
+
     }
 }
