aa3e9ef0a27b57526f21ab8b4d93ed1c03fcb84e
Dev Kumar <devkmandal0@gmail.com>
2025-05-16 19:56:34 +0530

    Solved using better approach

--- Arrays/MajorityElementsNby3.java
+++ Arrays/MajorityElementsNby3.java
@@ -1,13 +1,16 @@
 package Arrays;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MajorityElementsNby3 {
     public static void main(String[] args) {
 
         int[] arr={1,1,1,3,3,2,2,2};
         bruteForce(arr,arr.length);
+        betterAppr(arr,arr.length);
 
 
 
@@ -31,6 +34,33 @@ public class MajorityElementsNby3 {
         }
 
         System.out.println(result);
+    }
+
+
+    public static void betterAppr(int[] arr,int n){
+
+        Map<Integer,Integer> map = new HashMap<>();
+        List<Integer> result = new ArrayList<>();
+
+        for (int i = 0; i <n ; i++) {
+            if(map.containsKey(arr[i])){
+                map.put(arr[i],map.get(arr[i])+1);
+            }else{
+                map.put(arr[i],1);
+            }
+
+        }
+
+        for (Map.Entry<Integer,Integer> it:map.entrySet()){
+
+            if(it.getValue()>n/3){
+                result.add(it.getKey());
+            }
+
+        }
+
+        System.out.println(result);
+
     }
 
 
