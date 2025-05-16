a2b39e0132186f4b9159273f0603cf6294a4226d
Dev Kumar <devkmandal0@gmail.com>
2025-05-16 10:52:54 +0530

    Solved this quesion using Optimal approacah

---
+++ Arrays/SubarraySumEqualsK.java
@@ -0,0 +1,31 @@
+package Arrays;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SubarraySumEqualsK {
+    public static void main(String[] args) {
+
+        int[] arr = {1,2,3,-3,1,1,1,4,2,-3};
+
+
+
+    }
+
+    public static void optimalSol(int[] arr,int n,int k){
+        int prefixSum=0,count=0;
+        Map<Integer,Integer>map = new HashMap<>();
+        map.put(0,1);
+
+        for (int i = 0; i <n ; i++) {
+            prefixSum+=arr[i];
+            int remove =prefixSum-k;
+            count+=map.get(remove);
+            map.put(prefixSum,map.get(prefixSum)+1);
+
+        }
+
+        System.out.println(count);
+    }
+
+}
