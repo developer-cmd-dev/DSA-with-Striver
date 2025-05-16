154c6a35c0ff41e627efe448ba95c9fc5cf2e3a2
Dev Kumar <devkmandal0@gmail.com>
2025-05-16 19:15:47 +0530

    Solved print nth row of pascal triangle

--- Arrays/PascalTriangle.java
+++ Arrays/PascalTriangle.java
@@ -3,8 +3,8 @@ package Arrays;
 public class PascalTriangle {
     public static void main(String[] args) {
 
-        System.out.println(funcNcR(4,2));
-
+//        System.out.println(funcNcR(4,2));
+        printRow(6);
 
 
     }
@@ -21,5 +21,16 @@ public class PascalTriangle {
         return result;
 
 
+    }
+
+    //Print any nth row of pascal triangle
+    public static void printRow(int n){
+        int result=1;
+        System.out.print(result+" ");
+        for (int i = 1; i <n ; i++) {
+                result=result*(n-i);
+                result=result/i;
+            System.out.print(result+" ");
+        }
     }
 }
