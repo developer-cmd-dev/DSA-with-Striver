31322ade1f91f785440f8f82e0b70b2b2043da77
Dev Kumar <devkmandal0@gmail.com>
2025-06-03 12:10:56 +0530

    Solved using optimal solution 1

--- Arrays/MergeTwoSortedArrayWOExtraSpace.java
+++ Arrays/MergeTwoSortedArrayWOExtraSpace.java
@@ -7,6 +7,7 @@ public class MergeTwoSortedArrayWOExtraSpace {
 
         int[] arr1={1,3,5,7},arr2={0,2,6,8,9};
         bruteFroce(arr1,arr2,arr1.length,arr2.length);
+        optimalSol1(arr1,arr2,arr1.length,arr2.length);
 
 
     }
@@ -52,5 +53,29 @@ public class MergeTwoSortedArrayWOExtraSpace {
         System.out.println(Arrays.toString(temp));
 
 
+    }
+
+    public static void optimalSol1(int[] arr1,int[] arr2,int n,int m){
+        int left =n-1;
+        int right = 0;
+
+        while (left>=0 && right<m){
+            if(arr1[left]>arr2[right]){
+                swap(arr1,arr2,left,right);
+                left--;right++;
+            }else{
+                break;
+            }
+        }
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        System.out.println(Arrays.toString(arr1)+" : "+Arrays.toString(arr2));
+    }
+
+    private static void swap(int[] arr1, int[] arr2, int left, int right) {
+        int temp = arr1[left];
+        arr1[left]=arr2[right];
+        arr2[right]=temp;
     }
 }
