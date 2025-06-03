4b20a3682f2db587ae1997bd93de0de1edfec222
Dev Kumar <devkmandal0@gmail.com>
2025-06-03 13:20:48 +0530

    Solved using optimal solution 2 which is gap method

--- Arrays/MergeTwoSortedArrayWOExtraSpace.java
+++ Arrays/MergeTwoSortedArrayWOExtraSpace.java
@@ -6,8 +6,9 @@ public class MergeTwoSortedArrayWOExtraSpace {
     public static void main(String[] args) {
 
         int[] arr1={1,3,5,7},arr2={0,2,6,8,9};
-        bruteFroce(arr1,arr2,arr1.length,arr2.length);
-        optimalSol1(arr1,arr2,arr1.length,arr2.length);
+//        bruteFroce(arr1,arr2,arr1.length,arr2.length);
+//        optimalSol1(arr1,arr2,arr1.length,arr2.length);
+        optimalSol2(arr1,arr2,arr1.length,arr2.length);
 
 
     }
@@ -56,6 +57,7 @@ public class MergeTwoSortedArrayWOExtraSpace {
     }
 
     public static void optimalSol1(int[] arr1,int[] arr2,int n,int m){
+
         int left =n-1;
         int right = 0;
 
@@ -73,9 +75,42 @@ public class MergeTwoSortedArrayWOExtraSpace {
         System.out.println(Arrays.toString(arr1)+" : "+Arrays.toString(arr2));
     }
 
+    public static void optimalSol2(int[] arr1,int[] arr2,int n,int m){
+        int len = (n+m);
+        int gap = (len/2)+(len%2);
+        while (gap>0){
+            int left=0;
+            int right = left+gap;
+            while (right<len){
+                if(right>=n && left<n){ //arr1 and arr2
+                swapIfGreater(arr1,arr2,left,right-n);
+                } else if (left>=n) { // arr2 and arr 2
+                    swap(arr1,arr2,left-n,right-n);
+                }else{  // arr1 and arr1
+                    swap(arr1,arr2,left,right);
+                }
+                right++;
+                left++;
+            }
+            if (gap==1) break;
+            gap=(gap/2)+(gap%2);
+        }
+        System.out.println(Arrays.toString(arr1)+" : "+Arrays.toString(arr2));
+
+
+    }
+
+    private static void swapIfGreater(int[] arr1, int[] arr2, int left, int right){
+        if(arr1[left]>arr2[right]){
+            swap(arr1,arr2,left,right);
+        }
+    }
+
     private static void swap(int[] arr1, int[] arr2, int left, int right) {
         int temp = arr1[left];
         arr1[left]=arr2[right];
         arr2[right]=temp;
     }
+
+
 }
