15910dbe5370a2906d80acb332f454667097bc8a
Dev Kumar <devkmandal0@gmail.com>
2025-06-05 10:26:37 +0530

    Solved this question using optimal approach

--- Arrays/FindTheRepeatingAndMissingNumber.java
+++ Arrays/FindTheRepeatingAndMissingNumber.java
@@ -9,6 +9,7 @@ public class FindTheRepeatingAndMissingNumber {
 
         int[] arr={4,3,6,2,1,1};
         betterSolution(arr,arr.length);
+        optimalUsingMath(arr,arr.length);
 
 
 
@@ -33,5 +34,22 @@ public class FindTheRepeatingAndMissingNumber {
         System.out.println(repeating+" "+missing);
 
         
+    }
+
+    public static void optimalUsingMath(int[] arr,int n){
+        long SN = ((long) n *(n+1))/2; // Sum of given n;
+        long S2N = ((long) n*(n+1)*(2L *n+1))/6;
+        long s=0,s2=0;
+        for (int i = 0; i <n ; i++) {
+            s+=arr[i];
+            s2+=((long) arr[i]*arr[i]);
+        }
+
+        long value1 = SN-s;
+        long value2 = S2N-s2;
+        value2 = value2/value1;
+        long x = (value1+value2)/2;
+        long y = x-value1;
+        System.out.println(x+" "+y);
     }
 }
