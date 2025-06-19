51b79bee46ae69df5c0943be9b2de4d74f4ef7b0
Dev Kumar <devkmandal0@gmail.com>
2025-06-19 11:33:10 +0530

    Solved using Recursive method

--- BinarySearch/BinarySearch.java
+++ BinarySearch/BinarySearch.java
@@ -5,25 +5,39 @@ public class BinarySearch {
         int[] arr = {3,4,6,7,8,9,12,17};
         int low = 0;
         int high = arr.length-1;
-        int target=13;
+        int target=12;
         System.out.println( binarySearch(arr,low,high,target));
+        System.out.println(recursiveWay(arr,low,high,target));
     }
 
     public static int binarySearch(int[] arr,int low ,int high,int target){
 
         while (low<=high){
             int mid = (low+high)/2;
+            if(arr[mid]==target) return mid;
             if(arr[mid]>target){
                 high=mid-1;
             } else if (arr[mid]<target) {
                 low=mid+1;
-            }else{
-                return mid;
             }
         }
 
 
 return -1;
+
+    }
+
+
+    public static int recursiveWay(int[] arr,int low ,int high,int target){
+        int mid = (low+high)/2;
+        if(arr[mid]==target) return mid;
+        if(low>=high) return -1;
+        if(arr[mid]>target){
+            high=mid-1;
+        }else{
+         low=mid+1;
+        }
+          return recursiveWay(arr,low,high,target);
 
     }
 }
