dcb12f59ffb25a0199e921b4d87e506a643acb51
Dev Kumar <devkmandal0@gmail.com>
2025-06-19 10:59:21 +0530

    Solved this question

---
+++ Arrays/KidsWithCandies.java
@@ -0,0 +1,19 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class KidsWithCandies {
+    public static void main(String[] args) {
+        int[] arr = {2,3,5,1,3};
+        int n=arr.length;
+        int extraCandies = 3;
+        solution(arr,n,extraCandies);
+
+    }
+
+    public static void solution(int[] arr,int n,int extraCandies){
+
+        System.out.println(Arrays.toString(arr));
+
+    }
+}
