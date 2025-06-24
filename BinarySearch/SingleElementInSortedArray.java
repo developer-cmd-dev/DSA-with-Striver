db15af1cf250d7eb37d6eea6bde44d98acb69486
Dev Kumar <devkmandal0@gmail.com>
2025-06-24 11:31:24 +0530

    Solved using binary search with O(logn) Time complexity

--- BinarySearch/SingleElementInSortedArray.java
+++ BinarySearch/SingleElementInSortedArray.java
@@ -5,6 +5,7 @@ public class SingleElementInSortedArray {
 
         int[] arr = {1,1,2,2,3,3,4,5,5,6,6,7,7};
         System.out.println(bruteForce(arr,arr.length));
+        System.out.println(binarySearch(arr,arr.length));
 
 
 
@@ -22,6 +23,32 @@ public class SingleElementInSortedArray {
 
         }
         return -1;
+    }
+
+
+    public static int binarySearch(int[] nums,int n){
+        if(n==1)return nums[0];
+        if(nums[0]!=nums[1]) return nums[0];
+        if(nums[n-1]!=nums[n-2]) return nums[n-1];
+
+        int low=1,high=n-2;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]) return nums[mid];
+
+            if((mid%2==1 && nums[mid]==nums[mid-1]) || (mid%2==0 && nums[mid]==nums[mid+1])){
+                low = mid+1;
+            }else{
+                high = mid-1;
+            }
+
+        }
+
+
+
+
+
+return -1;
     }
 
 }
