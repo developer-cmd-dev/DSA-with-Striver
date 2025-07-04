aed4f2fe9508ecd1eaf48127d1c9d7355f237b02
Dev Kumar <devkmandal0@gmail.com>
2025-07-04 21:28:12 +0530

    Solved using binary search

--- PracticeQuestions/MinimumNoOfDaysMBouquets.java
+++ PracticeQuestions/MinimumNoOfDaysMBouquets.java
@@ -7,9 +7,10 @@ public class MinimumNoOfDaysMBouquets {
     public static void main(String[] args) {
 
 
-        int[] arr = {7,7,7,7,13,11,13,7};
+        int[] arr = {7,7,7,7,13,11,12,7};
         int m=2,k=3;
         System.out.println(bruteForce(arr,m,k));
+        System.out.println(binarySearch(arr,m,k));
 
     }
 
@@ -26,9 +27,9 @@ public class MinimumNoOfDaysMBouquets {
         }
 
         for (int i = min; i <=max ; i++) {
-            int calculatedDays = calcDays(arr,i,k);
+            boolean calculatedDays = calcDays(arr,i,k,m);
 
-            if (calculatedDays==m) return i-1;
+            if (calculatedDays) return i;
             
         }
         return -1;
@@ -37,7 +38,28 @@ public class MinimumNoOfDaysMBouquets {
 
     }
 
-    public static int calcDays(int[] arr,int days,int k){
+    public static int binarySearch(int[] arr,int m,int k){
+        int max =Integer.MIN_VALUE;
+        int min =Integer.MAX_VALUE;
+
+        for (int j : arr) {
+            max = Math.max(j, max);
+            min = Math.min(j, min);
+
+        }
+        while (min<=max){
+            int mid=(min+max)/2;
+            if(calcDays(arr,mid,k,m)){
+                max=mid-1;
+            }else{
+                min=mid+1;
+            }
+        }
+
+        return min;
+    }
+
+    public static boolean calcDays(int[] arr,int days,int k,int m){
 
         int countBouq=0;
         int pairs=0;
@@ -53,7 +75,7 @@ public class MinimumNoOfDaysMBouquets {
         }
         countBouq+=(pairs/k);
 
-        return countBouq;
+        return countBouq>=m;
 
     }
 }
