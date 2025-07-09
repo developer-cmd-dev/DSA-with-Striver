81e43aa66383b213190b98126e5dfdb800c347e4
Dev Kumar <devkmandal0@gmail.com>
2025-07-09 19:11:18 +0530

    Solved using binary search

--- PracticeQuestions/BookAllocation.java
+++ PracticeQuestions/BookAllocation.java
@@ -5,6 +5,7 @@ public class BookAllocation {
 
         int[] arr={25,46,28,49,24};
         System.out.println(bruteForce(arr,4));
+        System.out.println(binarySearch(arr,4));
 
 
 
@@ -33,6 +34,31 @@ public class BookAllocation {
 
 
 
+
+    }
+
+    public static int binarySearch(int[] arr,int students){
+        int n = arr.length;
+
+        if (students>n) return -1;
+        int low = Integer.MIN_VALUE,high=0;
+
+        for (int elem:arr) {
+            low = Math.max(elem,low);
+            high+=elem;
+        }
+
+        while (low<=high){
+            int mid = (low+high)/2;
+            int countStudents = count(arr,mid);
+            if (countStudents>students){
+                low=mid+1;
+            }else{
+                high=mid-1;
+            }
+        }
+
+        return low;
 
     }
 
