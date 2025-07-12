9d46ad2578fcbb9fb10142b68c4130b8fcff77b1
Dev Kumar <devkmandal0@gmail.com>
2025-07-12 19:01:01 +0530

    Solved using binary serach

--- FindRowWithMax1s.java
+++ FindRowWithMax1s.java
@@ -11,6 +11,7 @@ public class FindRowWithMax1s {
         int n=matrix.length;
         int m=matrix[0].length;
         System.out.println(bruteForce(matrix,n,m));
+        System.out.println(binarySearch(matrix,n,m));
 
 
 
@@ -33,6 +34,42 @@ public class FindRowWithMax1s {
         }
 
         return index;
+
+    }
+
+
+    public static int binarySearch(int[][] matrix,int n,int m){
+        int cntMax=-1;
+        int ind=-1;
+        for (int i = 0; i <n ; i++) {
+            int low =0;
+            int high=m-1;
+            int cnt =m - lowerBound(matrix[i],low,high,1);
+            if (cnt>cntMax){
+                cntMax=cnt;
+                ind=i;
+            }
+        }
+
+        return ind;
+
+    }
+
+    public static int lowerBound(int[] arr,int low ,int high,int target){
+        int ans = arr.length;
+        while (low<=high){
+            int mid = (low+high)/2;
+            if(arr[mid]>=target) {
+                ans = mid;
+                high=mid-1;
+            }else{
+                low=mid+1;
+            }
+
+        }
+
+
+        return ans;
 
     }
 }
