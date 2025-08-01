91cbe9b4b431c0dec8939ffeb669018b819a9306
Dev Kumar <devkmandal0@gmail.com>
2025-08-01 11:57:56 +0530

    Learnt Doubly Linked List

---
+++ LinkedList/DoublyLinkedList.java
@@ -0,0 +1,59 @@
+package LinkedList;
+
+
+
+class Node2{
+    int data;
+    Node2 next;
+    Node2 back;
+
+    public Node2(int data, Node2 next, Node2 back) {
+        this.data = data;
+        this.next = next;
+        this.back = back;
+    }
+
+    public Node2(int data){
+        this.data=data;
+        next=null;
+        back=null;
+    }
+}
+
+
+
+public class DoublyLinkedList {
+    public static void main(String[] args) {
+
+        int[] arr={1,2,3,4,5,6};
+        Node2 head=traverseArray(arr);
+        printLL(head);
+
+
+
+    }
+
+    public static void printLL(Node2 head){
+        Node2 temp = head;
+        System.out.println("Linked List");
+        while (temp!=null){
+            System.out.print(temp.data+"-> ");
+            temp=temp.next;
+        }
+        System.out.println();
+    }
+
+
+
+    public static Node2 traverseArray(int[] arr){
+        Node2 head=new Node2(arr[0]);
+        Node2 prev=head;
+        for (int i = 1; i <arr.length ; i++) {
+            Node2 temp=new Node2(arr[i],null,prev);
+            prev.next=temp;
+            prev=temp;
+
+        }
+        return head;
+    }
+}
